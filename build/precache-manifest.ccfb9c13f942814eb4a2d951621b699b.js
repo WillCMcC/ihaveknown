a8f0fefd20ae6b038a29188e1c3fe427b5b24f95
Will McCambley <willmccambley@Wills-MacBook-Pro-2.local>
2020-08-31 13:08:46 -0700

    ok

---
+++ build/precache-manifest.ccfb9c13f942814eb4a2d951621b699b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53200b926fe99230390602e6212b7d3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8af1d3ed91f2710b044b",
+    "url": "/static/css/main.c6600e3f.chunk.css"
+  },
+  {
+    "revision": "6a6fddb067dfd1e86aea",
+    "url": "/static/js/2.33b48294.chunk.js"
+  },
+  {
+    "revision": "2c598234c7a24970b5fdf032f602e7bc",
+    "url": "/static/js/2.33b48294.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8af1d3ed91f2710b044b",
+    "url": "/static/js/main.6d1f0cd6.chunk.js"
+  },
+  {
+    "revision": "101e8fda54a79c65b18d",
+    "url": "/static/js/runtime-main.5383d604.js"
+  }
+]);
